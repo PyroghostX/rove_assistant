7d89f37f86f1e78cd8db60c1b0e1cf0907907eb3
nearocodes <nearo.github@ortaisland.com>
2026-01-10 11:11:12 -0800

    Bump version

--- rove_assistant/lib/main.dart
+++ rove_assistant/lib/main.dart
@@ -33,7 +33,7 @@ void main() async {
 
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
-        create: (context) => RoveAppInfo(name: 'Assistant', version: '1.0.10')),
+        create: (context) => RoveAppInfo(name: 'Assistant', version: '1.0.11')),
     ChangeNotifierProvider(create: (context) => CampaignModel.instance),
     ChangeNotifierProvider(create: (context) => ItemsModel.instance),
     ChangeNotifierProvider(create: (context) => PlayersModel.instance),
